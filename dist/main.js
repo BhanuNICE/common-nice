7ab46142230741379f7b99869c09c6389286b5ff
BhanuNICE <chauhanbhanu185@gmail.com>
2025-05-22 10:55:04 +0000

    Remove dist from gitignore

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+import { jsx as o } from "react/jsx-runtime";
+import "react";
+const e = ({ label: t }) => o("button", { children: t });
+export {
+  e as Button
+};
+//# sourceMappingURL=main.js.map
