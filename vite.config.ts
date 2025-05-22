9c4273aed7e20ef1391eec21759dd2393998c397
BhanuNICE <chauhanbhanu185@gmail.com>
2025-05-22 09:59:43 +0000

    Configuration for lib

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts({ include: ["lib"] })],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.tsx"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
+    },
+    copyPublicDir: false,
+  },
+});
